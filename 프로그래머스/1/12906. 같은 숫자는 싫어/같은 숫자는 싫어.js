a3b4634d8eab844fc7c5a0b356699a68f52a8f0a
kiduki555 <125900064+kiduki555@users.noreply.github.com>
2024-11-27 17:05:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 52.68 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    const stack = [];
+    for (const num of arr) {
+        if (stack.length === 0 || stack[stack.length - 1] !== num) {
+            stack.push(num);
+        }
+    }
+    return stack;
+}
